f5410b432a3f2185743f126193666df17c931dfb
Vinicius Almeida <gvweb.solucoes@gmail.com>
2025-02-25 17:22:18 -0300

    Menu

--- src/app/painel/components/SiderBar.tsx
+++ src/app/painel/components/SiderBar.tsx
@@ -1,41 +1,40 @@
-"use client"
+"use client";
 
-import Image from "next/image"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { ScrollArea } from "@/components/ui/scroll-area"
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import {
-  Settings,
-  Users,
   ChevronRight,
-  Shield,
-  Mail,
   UserPlus,
+  Users,
   Menu,
   DockIcon,
   BoxIcon,
   BookXIcon,
   GraduationCap,
-  CalendarCheck
-} from "lucide-react"
-import { useState } from "react"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+  CalendarCheck,
+} from "lucide-react";
+import { useState } from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
-interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-interface MenuItem {
-  icon: React.ElementType
-  label: string
-  href: string
-  subItems?: { icon: React.ElementType; label: string; href: string }[]
+interface SidebarProps {
+  collapsed: boolean;
+  setCollapsed: (collapsed: boolean) => void;
 }
 
-export default function Sidebar({ className }: SidebarProps) {
-  const [collapsed, setCollapsed] = useState(false)
-  const [activeItem, setActiveItem] = useState<string | null>(null)
-  const pathname = usePathname()
+interface MenuItem {
+  icon: React.ElementType;
+  label: string;
+  href: string;
+  subItems?: { icon: React.ElementType; label: string; href: string }[];
+}
+
+export default function Sidebar({ collapsed, setCollapsed }: SidebarProps) {
+  const [activeItem, setActiveItem] = useState<string | null>(null);
+  const pathname = usePathname();
 
   const menuItems: MenuItem[] = [
     {
@@ -55,19 +54,11 @@ export default function Sidebar({ className }: SidebarProps) {
       label: "Usuários",
       href: "#",
       subItems: [
-        {
-          icon: UserPlus,
-          label: "Novo Usuário",
-          href: "/painel/usuarios/novo",
-        },
-        {
-          icon: Users,
-          label: "Gerenciar Usuários",
-          href: "/painel/usuarios",
-        },
+        { icon: UserPlus, label: "Novo Usuário", href: "/painel/usuarios/novo" },
+        { icon: Users, label: "Gerenciar Usuários", href: "/painel/usuarios" },
       ],
     },
-  ]
+  ];
 
   const renderMenuItem = (item: MenuItem, index: number) => {
     if (collapsed) {
@@ -97,7 +88,7 @@ export default function Sidebar({ className }: SidebarProps) {
                         variant="ghost"
                         className={cn(
                           "w-full justify-start py-1 px-3 text-sm hover:bg-gray-800 transition-colors hover:text-white text-white",
-                          pathname === subItem.href && "bg-gray-700",
+                          pathname === subItem.href && "bg-gray-700"
                         )}
                       >
                         <subItem.icon className="h-4 w-4 mr-2" />
@@ -110,29 +101,27 @@ export default function Sidebar({ className }: SidebarProps) {
             </div>
           </PopoverContent>
         </Popover>
-      )
+      );
     }
 
     return (
       <div key={index} className="mb-2">
-        <Link href={item.href} passHref>
-          <Button
-            variant="ghost"
-            className={cn(
-              "w-full justify-start py-2 px-3 text-left hover:bg-gray-800 transition-colors hover:text-white text-white",
-              pathname === item.href && "bg-gray-800",
-            )}
-            onClick={() => setActiveItem(activeItem === item.label ? null : item.label)}
-          >
-            <item.icon className="h-5 w-5 mr-2" />
-            <span>{item.label}</span>
-            {item.subItems && (
-              <ChevronRight
-                className={cn("h-4 w-4 ml-auto transition-transform", activeItem === item.label && "rotate-90")}
-              />
-            )}
-          </Button>
-        </Link>
+        <Button
+          variant="ghost"
+          className={cn(
+            "w-full justify-start py-2 px-3 text-left hover:bg-gray-800 transition-colors hover:text-white text-white",
+            pathname === item.href && "bg-gray-800"
+          )}
+          onClick={() => setActiveItem(activeItem === item.label ? null : item.label)}
+        >
+          <item.icon className="h-5 w-5 mr-2" />
+          <span>{item.label}</span>
+          {item.subItems && (
+            <ChevronRight
+              className={cn("h-4 w-4 ml-auto transition-transform", activeItem === item.label && "rotate-90")}
+            />
+          )}
+        </Button>
         {activeItem === item.label && item.subItems && (
           <div className="ml-6 mt-1 space-y-1">
             {item.subItems.map((subItem, subIndex) => (
@@ -141,7 +130,7 @@ export default function Sidebar({ className }: SidebarProps) {
                   variant="ghost"
                   className={cn(
                     "w-full justify-start py-1 px-3 text-sm hover:bg-gray-800 transition-colors hover:text-white text-white",
-                    pathname === subItem.href && "bg-gray-700",
+                    pathname === subItem.href && "bg-gray-700"
                   )}
                 >
                   <subItem.icon className="h-4 w-4 mr-2" />
@@ -152,23 +141,21 @@ export default function Sidebar({ className }: SidebarProps) {
           </div>
         )}
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div
       className={cn(
         "flex flex-col h-full bg-gray-900 text-gray-100 transition-all duration-300 ease-in-out",
-        collapsed ? "w-16" : "w-64",
-        className,
+        collapsed ? "w-16" : "w-64"
       )}
     >
       <div className="flex items-center justify-between p-4">
         {!collapsed ? (
           <Link href="/painel" className="flex items-center">
             <Image src="/logotipo.svg" alt="Grupo AAP-VR" width={40} height={40} className="mr-2" />
-            <span className="text-ml font-bold">AAP-VR / CPSI{/* <strong className="font-light"></strong> */}
-            </span>
+            <span className="text-ml font-bold">AAP-VR / CPSI</span>
           </Link>
         ) : (
           <div className="w-8 h-8" /> // Placeholder para manter o espaçamento
@@ -186,6 +173,5 @@ export default function Sidebar({ className }: SidebarProps) {
         <div className="px-3 py-2">{menuItems.map((item, index) => renderMenuItem(item, index))}</div>
       </ScrollArea>
     </div>
-  )
+  );
 }
-
