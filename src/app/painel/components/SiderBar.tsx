141352e1fe045b771e86e15dbc803256e0abbd63
mdutrashark <marcos.md4199@gmail.com>
2025-03-20 14:43:15 -0300

    Corrigindo icone do sub-menu Segurança

--- src/app/painel/components/SiderBar.tsx
+++ src/app/painel/components/SiderBar.tsx
@@ -18,9 +18,14 @@ import {
   BookXIcon,
   GraduationCap,
   CalendarCheck,
+  LockKeyhole,
 } from "lucide-react";
 import { useEffect, useState } from "react";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 interface SidebarProps {
   collapsed: boolean;
@@ -36,6 +41,7 @@ const iconMap: { [key: string]: React.ElementType } = {
   CalendarCheck,
   Users,
   UserPlus,
+  LockKeyhole,
 };
 
 interface MenuItem {
@@ -43,9 +49,9 @@ interface MenuItem {
   label: string;
   href: string;
   requiredGroups?: string[]; // Adicione essa linha
-  subItems?: { 
-    icon: React.ElementType; 
-    label: string; 
+  subItems?: {
+    icon: React.ElementType;
+    label: string;
     href: string;
     requiredGroups?: string[]; // Adicione aqui também para os subitens
   }[];
@@ -81,13 +87,13 @@ export default function Sidebar({ collapsed, setCollapsed }: SidebarProps) {
           }))
         : [],
     }));
-  
+
     setMenuItems(parsedMenu);
   }, []);
 
   const hasAccess = (requiredGroups?: string[]) => {
     if (!requiredGroups || requiredGroups.length === 0) return true;
-    return requiredGroups.some(group => userGroups.includes(group));
+    return requiredGroups.some((group) => userGroups.includes(group));
   };
 
   const renderMenuItem = (item: MenuItem, index: number) => {
@@ -104,7 +110,10 @@ export default function Sidebar({ collapsed, setCollapsed }: SidebarProps) {
               <item.icon className="h-5 w-5" />
             </Button>
           </PopoverTrigger>
-          <PopoverContent side="right" className="w-56 p-0 bg-gray-900 border-gray-800">
+          <PopoverContent
+            side="right"
+            className="w-56 p-0 bg-gray-900 border-gray-800"
+          >
             <div className="py-2">
               <Button
                 variant="ghost"
@@ -112,26 +121,31 @@ export default function Sidebar({ collapsed, setCollapsed }: SidebarProps) {
               >
                 <span>{item.label}</span>
               </Button>
-              {item.subItems && item.subItems.some(subItem => subItem.requiredGroups && hasAccess(subItem.requiredGroups)) && (
-                <div className="ml-3 mt-1 space-y-1">
-                  {item.subItems.map((subItem, subIndex) =>
-                    subItem.requiredGroups && hasAccess(subItem.requiredGroups) ? (
-                      <Link key={subIndex} href={subItem.href} passHref>
-                        <Button
-                          variant="ghost"
-                          className={cn(
-                            "w-full justify-start py-1 px-3 text-sm hover:bg-gray-800 transition-colors hover:text-white text-white",
-                            pathname === subItem.href && "bg-gray-700"
-                          )}
-                        >
-                          <subItem.icon className="h-4 w-4 mr-2" />
-                          <span>{subItem.label}</span>
-                        </Button>
-                      </Link>
-                    ) : null
-                  )}
-                </div>
-              )}
+              {item.subItems &&
+                item.subItems.some(
+                  (subItem) =>
+                    subItem.requiredGroups && hasAccess(subItem.requiredGroups)
+                ) && (
+                  <div className="ml-3 mt-1 space-y-1">
+                    {item.subItems.map((subItem, subIndex) =>
+                      subItem.requiredGroups &&
+                      hasAccess(subItem.requiredGroups) ? (
+                        <Link key={subIndex} href={subItem.href} passHref>
+                          <Button
+                            variant="ghost"
+                            className={cn(
+                              "w-full justify-start py-1 px-3 text-sm hover:bg-gray-800 transition-colors hover:text-white text-white",
+                              pathname === subItem.href && "bg-gray-700"
+                            )}
+                          >
+                            <subItem.icon className="h-4 w-4 mr-2" />
+                            <span>{subItem.label}</span>
+                          </Button>
+                        </Link>
+                      ) : null
+                    )}
+                  </div>
+                )}
             </div>
           </PopoverContent>
         </Popover>
@@ -146,19 +160,27 @@ export default function Sidebar({ collapsed, setCollapsed }: SidebarProps) {
             "w-full justify-start py-2 px-3 text-left hover:bg-gray-800 transition-colors hover:text-white text-white",
             pathname === item.href && "bg-gray-800"
           )}
-          onClick={() => setActiveItem(activeItem === item.label ? null : item.label)}
+          onClick={() =>
+            setActiveItem(activeItem === item.label ? null : item.label)
+          }
         >
           <item.icon className="h-5 w-5 mr-2" />
           <span>{item.label}</span>
           {item.subItems && (
             <ChevronRight
-              className={cn("h-4 w-4 ml-auto transition-transform", activeItem === item.label && "rotate-90")}
+              className={cn(
+                "h-4 w-4 ml-auto transition-transform",
+                activeItem === item.label && "rotate-90"
+              )}
             />
           )}
         </Button>
         {activeItem === item.label &&
           item.subItems &&
-          item.subItems.some(subItem => subItem.requiredGroups && hasAccess(subItem.requiredGroups)) && (
+          item.subItems.some(
+            (subItem) =>
+              subItem.requiredGroups && hasAccess(subItem.requiredGroups)
+          ) && (
             <div className="ml-6 mt-1 space-y-1">
               {item.subItems.map((subItem, subIndex) =>
                 subItem.requiredGroups && hasAccess(subItem.requiredGroups) ? (
@@ -180,7 +202,7 @@ export default function Sidebar({ collapsed, setCollapsed }: SidebarProps) {
           )}
       </div>
     );
-};
+  };
 
   return (
     <div
@@ -192,7 +214,13 @@ export default function Sidebar({ collapsed, setCollapsed }: SidebarProps) {
       <div className="flex items-center justify-between p-4">
         {!collapsed ? (
           <Link href="/painel" className="flex items-center">
-            <Image src="/logotipo.svg" alt="Grupo AAP-VR" width={40} height={40} className="mr-2" />
+            <Image
+              src="/logotipo.svg"
+              alt="Grupo AAP-VR"
+              width={40}
+              height={40}
+              className="mr-2"
+            />
             <span className="text-ml font-bold">AAP-VR / CPSI</span>
           </Link>
         ) : (
@@ -208,7 +236,9 @@ export default function Sidebar({ collapsed, setCollapsed }: SidebarProps) {
         </Button>
       </div>
       <ScrollArea className="flex-grow">
-        <div className="px-3 py-2">{menuItems.map((item, index) => renderMenuItem(item, index))}</div>
+        <div className="px-3 py-2">
+          {menuItems.map((item, index) => renderMenuItem(item, index))}
+        </div>
       </ScrollArea>
     </div>
   );
