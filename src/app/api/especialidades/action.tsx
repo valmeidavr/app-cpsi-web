eea36e93fd34d93b8b56977135c71d96b5240ea9
mdutrashark <marcos.md4199@gmail.com>
2025-03-26 22:13:04 -0300

    feat: Criando schema.ts e requisições da api

---
+++ src/app/api/especialidades/action.tsx
@@ -0,0 +1,81 @@
+"use server";
+
+import { http } from "@/util/http";
+import { revalidatePath } from "next/cache";
+import { toast } from "sonner";
+
+export async function getEspecialidades(
+  page: number = 1,
+  limit: number = 5,
+  search?: string
+) {
+  const { data } = await http.get("/especialidades", {
+    params: { page, limit, search },
+  });
+  return data;
+}
+
+type createEspecialidadePayload = {
+  nome: string;
+  codigo: string;
+};
+type updateEspecialidadePayload = {
+  nome: string;
+  codigo: string;
+};
+export async function createEspecialidade({
+  nome,
+  codigo,
+}: createEspecialidadePayload) {
+  try {
+    await http.post("/especialidades", {
+      nome,
+      codigo,
+    });
+    revalidatePath("/painel/especialidades");
+  } catch (error: any) {
+    console.error("Erro ao criar especialidade:", error);
+    toast.error(
+      error.response?.data?.message || "Erro ao criar especialidade."
+    );
+  }
+}
+
+export async function getEspecialidadeById(id: string) {
+  const { data } = await http.get(`http://localhost:3000/especialidades/${id}`);
+  return data;
+}
+
+export async function updateEspecialidade(
+  id: string,
+  body: updateEspecialidadePayload
+) {
+  try {
+    const { data } = await http.patch(
+      `http://localhost:3000/especialidades/${id}`,
+      body
+    );
+    revalidatePath("painel/especialidades?status=success");
+    return data;
+  } catch (error) {
+    console.error("Erro no update:", error);
+    return {
+      message: "Não foi possível fazer o update da Especialidade",
+      error: true,
+    };
+  }
+}
+
+export async function deleteEspecialidade(id: number) {
+  try {
+    const response = await http.delete(
+      `http://localhost:3000/especialidades/${id}`
+    );
+    revalidatePath("painel/especialidades");
+  } catch {
+    return {
+      message: "Não foi possível deletar a Especialidade",
+      error: true,
+    };
+  }
+}
---
+++ src/app/api/especialidades/schema/formSchemaEspecialidade.ts
@@ -0,0 +1,15 @@
+import * as z from "zod";
+
+export const formSchema = z.object({
+  nome: z
+    .string()
+    .min(3, { message: "O nome deve ter pelo menos 3 caracteres" })
+    .min(1, { message: "O campo é obrigatório" })
+    .regex(/^[a-zA-ZÀ-ÿ\s]+$/, {
+      message: "O nome não pode conter números ou símbolos",
+    }),
+
+  codigo: z.string().regex(/^\d+$/, {
+    message: "Apenas números são permitidos",
+  }),
+});
