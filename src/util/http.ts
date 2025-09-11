90d65aa3970615aade9d88b9ec84160aadb43270
MiguelFrog <miguelcardozo@frogsummit.com>
2025-09-11 09:15:51 -0300

    feature: ajuste

--- src/util/http.ts
+++ src/util/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getCookie } from "./cookies";
 export const http = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "/",
 });
 http.interceptors.request.use(async (request) => {
   let token = null;
